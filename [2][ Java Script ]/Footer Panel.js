20e9c09a9dcbe951d90e15e46dcdb7b252ee4438
Keiishkii <keiishkii@gmail.com>
2022-10-16 01:43:50 +0100

    INCISIV Template added

---
+++ [2][ Java Script ]/Footer Panel.js
@@ -0,0 +1,54 @@
+window.onload = function ()
+{
+    LoadNavigationPanel();
+};
+
+function LoadNavigationPanel()
+{
+    document.getElementById('navigation_panel').innerHTML = '<div class = "navigation_box">\n' +
+        '\t\t\t\t<div class = "navigation_bar">\n' +
+        '\t\t\t\t\t<div class = "navigation_bar_content_background">\n' +
+        '\t\t\t\t\t\t<ul>\n' +
+        '\t\t\t\t\t\t\t<li>\n' +
+        '\t\t\t\t\t\t\t\t<a href = "./index.html">\n' +
+        '\t\t\t\t\t\t\t\t\t<div class = "navigation_bar_content">\n' +
+        '\t\t\t\t\t\t\t\t\t\t<h1>Home</h1>\n' +
+        '\t\t\t\t\t\t\t\t\t</div>\n' +
+        '\t\t\t\t\t\t\t\t</a>\n' +
+        '\t\t\t\t\t\t\t</li>\n' +
+        '\t\t\t\t\t\t\t<li>\n' +
+        '\t\t\t\t\t\t\t\t<a href = "./bournemouth-university.html">\n' +
+        '\t\t\t\t\t\t\t\t\t<div class = "navigation_bar_content">\n' +
+        '\t\t\t\t\t\t\t\t\t\t<h1>Bournemouth University</h1>\n' +
+        '\t\t\t\t\t\t\t\t\t</div>\n' +
+        '\t\t\t\t\t\t\t\t</a>\n' +
+        '\t\t\t\t\t\t\t</li>\n' +
+        '\t\t\t\t\t\t\t<li>\n' +
+        '\t\t\t\t\t\t\t\t<a href="https://www.google.com">\n' +
+        '\t\t\t\t\t\t\t\t\t<div class = "navigation_bar_content">\n' +
+        '\t\t\t\t\t\t\t\t\t\t<h1>INCISIV</h1>\n' +
+        '\t\t\t\t\t\t\t\t\t</div>\n' +
+        '\t\t\t\t\t\t\t\t</a>\n' +
+        '\t\t\t\t\t\t\t</li>\n' +
+        '\t\t\t\t\t\t\t<li>\n' +
+        '\t\t\t\t\t\t\t\t<a href="https://www.google.com">\n' +
+        '\t\t\t\t\t\t\t\t\t<div class = "navigation_bar_content">\n' +
+        '\t\t\t\t\t\t\t\t\t\t<h1>BCHons</h1>\n' +
+        '\t\t\t\t\t\t\t\t\t</div>\n' +
+        '\t\t\t\t\t\t\t\t</a>\n' +
+        '\t\t\t\t\t\t\t</li>\n' +
+        '\t\t\t\t\t\t\t<li>\n' +
+        '\t\t\t\t\t\t\t\t<a href="https://www.google.com">\n' +
+        '\t\t\t\t\t\t\t\t\t<div class = "navigation_bar_content">\n' +
+        '\t\t\t\t\t\t\t\t\t\t<h1>Projects</h1>\n' +
+        '\t\t\t\t\t\t\t\t\t</div>\n' +
+        '\t\t\t\t\t\t\t\t</a>\n' +
+        '\t\t\t\t\t\t\t</li>\n' +
+        '\t\t\t\t\t\t\t<li>\n' +
+        '\t\t\t\t\t\t\t</li>\n' +
+        '\t\t\t\t\t\t</ul>\n' +
+        '\t\t\t\t\t\t<div class = "navigation_edge"></div>\n' +
+        '\t\t\t\t\t</div>\n' +
+        '\t\t\t\t</div>\n' +
+        '\t\t\t</div>'
+}
